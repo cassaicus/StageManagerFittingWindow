c59d739bf4b5f1d6c8da5ca1bb647c2fc6d591f4
cassaicus <info@cassaicus.com>
2022-11-13 09:03:10 +0900

    Initial Commit

---
+++ StageManagerFittingWindow/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  StageManagerFittingWindow
+//
+//  Created by ibis on 2022/11/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StageManagerFittingWindow/StageManagerFittingWindowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StageManagerFittingWindowApp.swift
+//  StageManagerFittingWindow
+//
+//  Created by ibis on 2022/11/13.
+//
+
+import SwiftUI
+
+@main
+struct StageManagerFittingWindowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
